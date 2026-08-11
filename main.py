b8ea55d388c3de6759df768382124259e134571b
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-11 20:23:20 +1000

    variables and string interpolation

---
+++ main.py
@@ -0,0 +1,17 @@
+# variables
+
+# num1 = 2
+# num2 = 4
+# print(num1 + num2)
+# num2 = 10
+# print(num1 + num2)
+
+# string interpolation
+
+name = 'Jean'
+age = 15
+print(f'my name is {name} and I am {age}')
+
+a = 10
+b = 20
+print(f'the Sum of the above variables is {a + b}')
