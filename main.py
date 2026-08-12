6b65507eca49922093a6172d886e5cbb5948b213
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-12 21:11:25 +1000

    split and join methods

--- main.py
+++ main.py
@@ -8,10 +8,26 @@
 
 # string interpolation
 
-name = 'Jean'
-age = 15
-print(f'my name is {name} and I am {age}')
+# name = 'Jean'
+# age = 15
+# print(f'my name is {name} and I am {age}')
 
-a = 10
-b = 20
-print(f'the Sum of the above variables is {a + b}')
+# a = 10
+# b = 20
+# print(f'the Sum of the above variables is {a + b}')
+
+# string_methods
+# len
+
+# name = 'Esther'
+# print(len(name))
+
+# name = "Hello There Everyone"
+# print(name.split('e'))
+
+# s = 'abc, '
+# print(s.split(','))
+
+s = ['H', 'llo Th', 'r', ' Ev', 'ryon', '']
+print('e'.join(s))
+
