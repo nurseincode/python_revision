5f95f640fa67a083d2e1137c0199c4f498c64acc
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-15 20:06:58 +1000

    loops completed

--- main.py
+++ main.py
@@ -62,3 +62,72 @@
 #         break
 #     print(num) 
 
+# is_raining = False
+
+# if is_raining:
+#     print('carry an umbrella!')
+# else:
+#     print('you don\'t need an umbrella')
+
+# score = 70
+
+# if score >=90:
+#     print('Grade A')
+# elif score >=80:
+#     print('Grade B')
+# else:
+#     print('Grade C') # this block will execute
+
+# day_of_the_week = 5
+
+# if day_of_the_week ==1:
+#     print('today is Monday')
+# elif day_of_the_week ==2:
+#     print('today is Tuesday')
+# elif day_of_the_week ==3:
+#     print('today is Wednesday')
+# elif day_of_the_week ==4:
+#     print('today is Thursday')
+# else: 
+#     print('today is Friday')
+
+
+
+# day_of_week = 10
+
+# match day_of_week:
+#     case 1:
+#         print('Monday')
+#     case 2:
+#         print('Tuesday')
+#     case 3:
+#         print('Wednesday')
+#     case _:
+#         print('incorrect input')
+
+# match day_of_week:
+#     case 1 | 2 | 3 | 4 | 5:
+#         print('Weekday')
+#     case 6 | 7:
+#         print('Weekend')
+#     case _:
+#         print('error: not a valid day of the week')
+
+
+
+is_raining = False
+
+# if is_raining:
+#     print('carry an umbrella!')
+# else:
+#     print('no need for an umbrella')
+
+# # ternary expression
+# print('carry an umbrella' if is_raining else 'no need for an umbrella')
+
+
+
+
+          
+
+
