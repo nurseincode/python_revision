4ee1e42b68bb90cf57b8d4437a5e768c690eb793
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-18 20:07:53 +1000

    methods

--- list_tuples.py
+++ list_tuples.py
@@ -8,5 +8,28 @@ numbers = [5, 23, 32, 40]
 
 # print(numbers[-2]) 
 
-numbers.append(50)
+# numbers.append(5)
+# print(numbers)
+
+# numbers.insert(2, 70)
+# print(numbers)
+
+# numbers.pop()
+# print(numbers)
+
+# popped_value = numbers.pop(2)
+# print(numbers)
+# print(popped_value)
+
+# numbers.remove(5)
+# print(numbers)
+
+# numbers.sort()
+# print(numbers)
+
+numbers.sort(reverse = True)
 print(numbers)
+
+
+
+
