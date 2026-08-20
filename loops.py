882872b3cf4436ee1d6255821eb03f237fbf6b47
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-20 18:55:16 +1000

    for and while loop

--- loops.py
+++ loops.py
@@ -1,6 +1,27 @@
 # while loop
 
-number = 1
-while number <= 5:
-    print(number)
-    number +=1 
+# number = 1
+# while number <= 5:
+#     print(number)
+#     number +=1 
+
+# for loop
+
+names = ['Dee', 'Dot', 'Dop', 'Tee']
+
+# for name in names:
+#     print(name)
+
+for name in enumerate(names):
+    print(name)
+
+# fruits = ['apple', 'banana', 'cherry']
+# for fruit in enumerate(fruits):
+#     print(fruit)
+
+# fruits = ['apple', 'banana', 'cherry']
+# for index, fruit in enumerate(fruits):
+#     print(index, fruit)
+
+for index, name in enumerate(names):
+    print(f'{index+1}. {name}')
