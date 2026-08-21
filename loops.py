7156c199e351b6c27ff3c0bce84133420b22ec1a
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-21 20:25:16 +1000

    for loop ctd

--- loops.py
+++ loops.py
@@ -7,13 +7,13 @@
 
 # for loop
 
-names = ['Dee', 'Dot', 'Dop', 'Tee']
+# names = ['Dee', 'Dot', 'Dop', 'Tee']
 
 # for name in names:
 #     print(name)
 
-for name in enumerate(names):
-    print(name)
+# for name in enumerate(names):
+#     print(name)
 
 # fruits = ['apple', 'banana', 'cherry']
 # for fruit in enumerate(fruits):
@@ -23,5 +23,22 @@ for name in enumerate(names):
 # for index, fruit in enumerate(fruits):
 #     print(index, fruit)
 
-for index, name in enumerate(names):
-    print(f'{index+1}. {name}')
+# for index, name in enumerate(names):
+#     print(f'{index+1}. {name}')
+
+# List all numbers between 0 to 20 and state whether odd or even
+
+# for i in range(0, 21):
+#     if i % 2 == 0:
+#         print(f'{i} is even')
+#     else:
+#         print(f'{i} is odd')
+
+for num in range(0,21):
+    print(f'{num} is {'even' if num % 2 == 0 else 'odd'}')
+   
+       
+
+
+
+
