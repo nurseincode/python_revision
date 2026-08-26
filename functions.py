5e8e603e69b6263f4a6d0e64cc9fa1ca3bf58251
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-26 18:47:28 +1000

    keyword and positional arguments

--- functions.py
+++ functions.py
@@ -3,7 +3,24 @@
 #     print('goodbye')
 # greet()
 
-def add_numbers(): 
-    return 5 + 3
-result = add_numbers()
-print(result)
+# def add_numbers(): 
+#     return 5 + 3
+# result = add_numbers()
+# print(result)
+
+# Declare a function
+
+
+# def greet(name):
+#     print(f'Hello, {name}!')
+# greet("Dee")
+
+# greet(name=”Dee”)
+
+def greet(name, age):
+    print(f'Hello, {name}! You are {age} years old')
+greet("Dee", 20)
+
+  
+
+
