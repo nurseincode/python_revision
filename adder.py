33cc0da033ffcaeac3e93ef48deb309f023ba714
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-28 18:24:26 +1000

    basic functions completed

--- adder.py
+++ adder.py
@@ -1,10 +1,15 @@
 def add(num1, num2):
-    sum = num1 + num2
-    return sum
+    # sum = num1 + num2
+    # return sum
+    return num1 + num2
 
-total = add(10, 20)
-print(total)
-total2 = total * 2
-print(total2)
+# total = add(10, 20)
+# print(total)
+# final = add(total, 40)
+# print(final)
+# total2 = total * 2
+# print(total2)
 
+final = add(add(10, 20), 40)
+print(final)
 
